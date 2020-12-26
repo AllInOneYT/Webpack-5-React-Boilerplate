c8d2b3c5ab4825e38555384975f778aa21b6f7ac
Edy Gunawan <gunawanedy40@gmail.com>
2020-12-26 22:51:13 +0700

    added scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './scss/main.scss'
+
 // eslint-disable-next-line import/extensions
 import App from './components/App.jsx';
 
